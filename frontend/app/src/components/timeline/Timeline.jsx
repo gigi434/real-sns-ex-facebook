44d486bc44c24d8d7491cb151e9a80825594edee
wagatsuma.yuichi <firefly.of.april@gmail.com>
2022-09-24 03:00:09 +0900

    [fix] #10 ユーザーのIDを用いてタイムラインの表示を動的に変更できなかったため機能を修正した

--- frontend/app/src/components/timeline/Timeline.jsx
+++ frontend/app/src/components/timeline/Timeline.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import axios from "axios";
 
 import "./Timeline.css";
@@ -7,17 +8,18 @@ import Post from "../post/Post";
 
 export default function Timeline({ username }) {
     const [posts, setPosts] = useState([]);
+    const user = useSelector(state => state.auth.user);
 
     // timelineの取得を一回だけ実行する
     useEffect(() => {
         const fetchPosts = async () => {
             const response = username 
-                ? await axios.get(`/posts/profile/${username}`)
-                : await axios.get("/posts/timeline/6314be3ce9738babfe4fa4fd");
+                ? await axios.get(`/posts/profile/${username}`) //プロフィール画面を表示した場合
+                : await axios.get(`/posts/timeline/${user._id}`); // ホーム画面を表示した場合
             setPosts(response.data);
         }
         fetchPosts();
-    }, [])
+    }, [username, user._id])
     return (
         <section className="timeline">
             <div className="timeline-wrapper">
